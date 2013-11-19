d87cacbb9626956eb624e8a7e40e0c7a44726966
dramosti <diego@hlp.com.br>
2013-11-19 14:56:37 -0200

    pesquisa

---
+++ HLP.Wcf.Entries/IserviceTipo_produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using HLP.Entries.Model.Comercial;
+
+namespace HLP.Wcf.Entries
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IserviceTipo_produto" in both code and config file together.
+    [ServiceContract]
+    public interface IserviceTipo_produto
+    {
+        [OperationContract]
+        Tipo_produtoModel GetTipo(int idTipoProduto);
+        [OperationContract]
+        void Save(Tipo_produtoModel tipo);
+        [OperationContract]
+        void Delete(int idTipoProduto);
+        [OperationContract]
+        int Copy(int idTipoProduto);
+    }
+}
---
+++ HLP.Wcf.Entries/serviceTipo_produto.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using HLP.Entries.Model.Comercial;
+using HLP.Entries.Model.Repository.Interfaces.Comercial;
+using Ninject;
+
+namespace HLP.Wcf.Entries
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "serviceTipo_produto" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select serviceTipo_produto.svc or serviceTipo_produto.svc.cs at the Solution Explorer and start debugging.
+    public class serviceTipo_produto : IserviceTipo_produto
+    {
+        [Inject]
+        public ITipo_produtoRepository iTipo_produtoRepository { get; set; }
+
+        public Tipo_produtoModel GetTipo(int idTipoProduto)
+        {
+            return iTipo_produtoRepository.GetTipo(idTipoProduto);
+        }
+
+        public void Save(Tipo_produtoModel tipo)
+        {
+            iTipo_produtoRepository.Save(tipo);
+        }
+
+        public void Delete(int idTipoProduto)
+        {
+            iTipo_produtoRepository.Delete(idTipoProduto);
+        }
+
+        public int Copy(int idTipoProduto)
+        {
+           return iTipo_produtoRepository.Copy(idTipoProduto);
+        }
+    }
+}
