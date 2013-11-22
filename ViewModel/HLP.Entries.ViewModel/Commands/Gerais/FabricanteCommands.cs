760f0d2f6afd67793b59bf42489b5738d25a6177
armagni <mario@hlp.com.br>
2013-11-22 14:01:25 -0200

    Término de cadastro de Fabricante

---
+++ ViewModel/HLP.Entries.ViewModel/Commands/Gerais/FabricanteCommands.cs
@@ -0,0 +1,202 @@
+﻿using HLP.Comum.ViewModel.Commands;
+using HLP.Entries.Model.Models.Gerais;
+using HLP.Entries.ViewModel.ViewModels.Gerais;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HLP.Entries.ViewModel.Commands.Gerais
+{
+    public class FabricanteCommands
+    {
+        FabricanteViewModel objViewModel;
+        fabricanteService.IserviceFabricanteClient servico = new fabricanteService.IserviceFabricanteClient();
+
+        public FabricanteCommands(FabricanteViewModel objViewModel)
+        {
+
+            this.objViewModel = objViewModel;
+
+            this.objViewModel.commandDeletar = new RelayCommand(paramExec => Delete(),
+                    paramCanExec => DeleteCanExecute());
+
+            this.objViewModel.commandSalvar = new RelayCommand(paramExec => Save(),
+                    paramCanExec => SaveCanExecute(paramCanExec));
+
+            this.objViewModel.commandNovo = new RelayCommand(execute: paramExec => this.Novo(),
+                   canExecute: paramCanExec => this.NovoCanExecute());
+
+            this.objViewModel.commandAlterar = new RelayCommand(execute: paramExec => this.Alterar(),
+                    canExecute: paramCanExec => this.AlterarCanExecute());
+
+            this.objViewModel.commandCancelar = new RelayCommand(execute: paramExec => this.Cancelar(),
+                    canExecute: paramCanExec => this.CancelarCanExecute());
+
+            this.objViewModel.commandCopiar = new RelayCommand(execute: paramExec => this.Copy(),
+            canExecute: paramCanExec => this.CopyCanExecute());
+
+            this.objViewModel.commandPesquisar = new RelayCommand(execute: paramExec => this.ExecPesquisa(),
+                        canExecute: paramCanExec => true);
+
+            this.objViewModel.navegarCommand = new RelayCommand(execute: paramExec => this.Navegar(ContentBotao: paramExec),
+                canExecute: paramCanExec => objViewModel.navegarBaseCommand.CanExecute(paramCanExec));
+
+
+        }
+
+
+        #region Implementação Commands
+
+        public async void Save()
+        {
+            try
+            {
+                this.objViewModel.currentModel.idFabricante = await servico.saveFabricanteAsync(objFabricante:
+                    this.objViewModel.currentModel);
+                this.objViewModel.salvarBaseCommand.Execute(parameter: null);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        private bool SaveCanExecute(object objDependency)
+        {
+            if (objViewModel.currentModel == null || objDependency == null)
+                return false;
+
+            return (this.objViewModel.salvarBaseCommand.CanExecute(parameter: null)
+                && this.objViewModel.IsValid(objDependency as Panel));
+        }
+
+        public async void Delete()
+        {
+            try
+            {
+                if (MessageBox.Show(messageBoxText: "Deseja excluir o cadastro?",
+                    caption: "Excluir?", button: MessageBoxButton.YesNo, icon: MessageBoxImage.Question)
+                    == MessageBoxResult.Yes)
+                {
+                    if (await this.servico.deleteFabricanteAsync(idFabricante: (int)this.objViewModel.currentModel.idFabricante))
+                    {
+                        MessageBox.Show(messageBoxText: "Cadastro excluido com sucesso!", caption: "Ok",
+                            button: MessageBoxButton.OK, icon: MessageBoxImage.Information);
+                        this.objViewModel.currentModel = null;
+                    }
+                    else
+                    {
+                        MessageBox.Show(messageBoxText: "Não foi possível excluir o cadastro!", caption: "Falha",
+                            button: MessageBoxButton.OK, icon: MessageBoxImage.Exclamation);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.objViewModel.deletarBaseCommand.Execute(parameter: null);
+            }
+        }
+
+        private bool DeleteCanExecute()
+        {
+            if (objViewModel.currentModel == null)
+                return false;
+
+            return this.objViewModel.deletarBaseCommand.CanExecute(parameter: null);
+        }
+
+        private void Novo()
+        {
+            this.objViewModel.currentModel = new FabricanteModel();
+            this.objViewModel.novoBaseCommand.Execute(parameter: null);
+        }
+        private bool NovoCanExecute()
+        {
+            return this.objViewModel.novoBaseCommand.CanExecute(parameter: null);
+        }
+
+        private void Alterar()
+        {
+            this.objViewModel.alterarBaseCommand.Execute(parameter: null);
+        }
+        private bool AlterarCanExecute()
+        {
+            return this.objViewModel.alterarBaseCommand.CanExecute(parameter: null);
+        }
+
+        private void Cancelar()
+        {
+            this.objViewModel.currentModel = null;
+            this.objViewModel.cancelarBaseCommand.Execute(parameter: null);
+        }
+        private bool CancelarCanExecute()
+        {
+            return this.objViewModel.cancelarBaseCommand.CanExecute(parameter: null);
+        }
+
+        public async void Copy()
+        {
+            try
+            {
+                this.objViewModel.currentModel.idFabricante = await this.servico.copyFabricanteAsync(idFabricante:
+                    (int)this.objViewModel.currentModel.idFabricante);
+                this.objViewModel.copyBaseCommand.Execute(null);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public bool CopyCanExecute()
+        {
+            return this.objViewModel.copyBaseCommand.CanExecute(null);
+        }
+
+        public void Navegar(object ContentBotao)
+        {
+            try
+            {
+                objViewModel.navegarBaseCommand.Execute(ContentBotao);
+                this.PesquisarRegistro();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ExecPesquisa()
+        {
+            this.objViewModel.pesquisarBaseCommand.Execute(null);
+            this.PesquisarRegistro();
+        }
+
+        private void PesquisarRegistro()
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += new DoWorkEventHandler(this.metodoGetModel);
+            bw.RunWorkerAsync();
+
+        }
+
+        private async void metodoGetModel(object sender, DoWorkEventArgs e)
+        {
+            this.objViewModel.currentModel = await this.servico.getFabricanteAsync(idFabricante:
+                this.objViewModel.currentID);
+        }
+        #endregion
+
+
+    }
+}
