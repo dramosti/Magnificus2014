fb1a66ad955df489ea7f1eacc6fa045db469b5f4
dramosti <diego@hlp.com.br>
2013-11-22 15:06:30 -0200

    a

---
+++ ViewModel/HLP.Entries.ViewModel/Commands/Transportes/ModoEntregaCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HLP.Entries.ViewModel.ViewModels.Transportes;
+
+namespace HLP.Entries.ViewModel.Commands.Transportes
+{
+    public class ModoEntregaCommands
+    {
+
+        ModoEntregaViewModel objViewModel;
+
+        public ModoEntregaCommands(ModoEntregaViewModel objViewModel) 
+        {
+            this.objViewModel = objViewModel;
+        }
+
+    }
+}
