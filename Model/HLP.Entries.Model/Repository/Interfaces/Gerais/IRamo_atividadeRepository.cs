ccf5ef285a8b150ada642195daa03265724d7890
armagni <mario@hlp.com.br>
2013-11-22 14:52:38 -0200

    Wcf Ramo Atividade

---
+++ Model/HLP.Entries.Model/Repository/Interfaces/Gerais/IRamo_atividadeRepository.cs
@@ -0,0 +1,17 @@
+﻿using HLP.Entries.Model.Models.Gerais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Entries.Model.Repository.Interfaces.Gerais
+{
+    public interface IRamo_atividadeRepository
+    {
+        Ramo_atividadeModel GetRamo(int idRamoAtividade);
+        void Save(Ramo_atividadeModel ramo);
+        void Delete(int idRamoAtividade);
+        int Copy(int idRamoAtividade);
+    }
+}
