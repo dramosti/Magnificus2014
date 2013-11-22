46f5e501fd9509cb9b33cc1472322ac2dd8ded42
armagni <mario@hlp.com.br>
2013-11-22 15:02:12 -0200

    Conclusão de cadastro de Ramo de Atividade

---
+++ ViewModel/HLP.Entries.ViewModel/ViewModels/Gerais/Ramo_atividadeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using HLP.Comum.ViewModel.ViewModels;
+using HLP.Entries.Model.Models.Gerais;
+using HLP.Entries.ViewModel.Commands.Gerais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HLP.Entries.ViewModel.ViewModels.Gerais
+{
+    public class Ramo_atividadeViewModel : ViewModelBase
+    {
+
+        #region Icommands
+        public ICommand commandSalvar { get; set; }
+        public ICommand commandDeletar { get; set; }
+        public ICommand commandNovo { get; set; }
+        public ICommand commandAlterar { get; set; }
+        public ICommand commandCancelar { get; set; }
+        public ICommand commandCopiar { get; set; }
+        public ICommand commandPesquisar { get; set; }
+        public ICommand navegarCommand { get; set; }
+        #endregion
+
+
+        public Ramo_atividadeViewModel()
+        {
+            Ramo_atividadeCommands comm = new Ramo_atividadeCommands(objViewModel: this);
+        }
+
+        private Ramo_atividadeModel _currentModel;
+
+        public Ramo_atividadeModel currentModel
+        {
+            get { return _currentModel; }
+            set
+            {
+                _currentModel = value;
+                base.NotifyPropertyChanged(propertyName: "currentModel");
+            }
+        }
+    }
+}
