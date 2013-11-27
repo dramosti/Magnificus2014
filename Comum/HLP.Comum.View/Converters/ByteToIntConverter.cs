f73e0c436d489e10097b845af24f880c80aa3d1d
armagni <mario@hlp.com.br>
2013-11-27 13:23:09 -0200

    Inclusão de referencia de wcf

---
+++ Comum/HLP.Comum.View/Converters/ByteToIntConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HLP.Comum.View.Converters
+{
+    public class ByteToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int res;
+
+            if (int.TryParse(value.ToString(), out res))
+                return res;
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            byte res;
+
+            if (byte.TryParse(value.ToString(), out res))
+                return res;
+
+            return null;
+        }
+    }
+}
