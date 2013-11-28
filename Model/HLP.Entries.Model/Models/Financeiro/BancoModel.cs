2a23826ae2140f86b03f2b11c2e6a7ff2d3376f5
armagni <mario@hlp.com.br>
2013-11-28 16:21:20 -0200

    Inclusão de referencia wcf Rota

---
+++ Model/HLP.Entries.Model/Models/Financeiro/BancoModel.cs
@@ -0,0 +1,55 @@
+﻿using HLP.Comum.Infrastructure;
+using HLP.Comum.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Entries.Model.Models.Financeiro
+{
+    public partial class BancoModel : modelBase
+    {
+        public BancoModel()
+            : base(xTabela: "Banco")
+        {
+        }
+
+
+        private int? _idBanco;
+        [ParameterOrder(Order = 1)]
+        public int? idBanco
+        {
+            get { return _idBanco; }
+            set
+            {
+                _idBanco = value;
+                base.NotifyPropertyChanged(propertyName: "idBanco");
+            }
+        }
+
+        [ParameterOrder(Order = 2)]
+        public string cBanco { get; set; }
+        [ParameterOrder(Order = 3)]
+        public string xBancoResumido { get; set; }
+        [ParameterOrder(Order = 4)]
+        public string xBanco { get; set; }
+        [ParameterOrder(Order = 5)]
+        public string xLinkLogo { get; set; }
+        [ParameterOrder(Order = 6)]
+        public byte vDxMais { get; set; }
+        [ParameterOrder(Order = 7)]
+        public byte vDxMenos { get; set; }
+    }
+
+    public partial class BancoModel
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                return base[columnName];
+            }
+        }
+    }
+}
