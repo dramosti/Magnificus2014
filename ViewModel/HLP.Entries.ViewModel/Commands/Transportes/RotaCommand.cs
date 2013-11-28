fdb3f059c43fd765fb79c8e5f61f7d14c4176f58
dramosti <diego@hlp.com.br>
2013-11-28 16:12:00 -0200

    a

---
+++ ViewModel/HLP.Entries.ViewModel/Commands/Transportes/RotaCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Entries.ViewModel.Commands.Transportes
+{
+   public class RotaCommand
+    {
+
+       public RotaCommand() 
+       {
+
+       }
+    }
+}
