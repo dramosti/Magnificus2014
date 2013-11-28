76948a5a9b9ada95d2c3847a755aab035a6115a7
dramosti <diego@hlp.com.br>
2013-11-28 09:40:44 -0200

    PlanoPagamentoCommand.cs

---
+++ ViewModel/HLP.Entries.ViewModel/Commands/Gerais/PlanoPagamentoCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HLP.Entries.ViewModel.ViewModels.Gerais;
+
+namespace HLP.Entries.ViewModel.Commands.Gerais
+{
+    public class PlanoPagamentoCommand
+    {
+        PlanoPagamentoViewModel objViewModel;
+        public PlanoPagamentoCommand(PlanoPagamentoViewModel objViewModel) 
+        {
+            this.objViewModel = objViewModel;
+
+        }
+    }
+}
