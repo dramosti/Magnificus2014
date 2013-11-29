e2993ba2e7c50641d5b4df7ffdb2d9b33aaf1211
armagni <mario@hlp.com.br>
2013-11-29 08:26:23 -0200

    Inclusão wcf de st Cofins

---
+++ Model/HLP.Entries.Model/Models/Fiscal/Situacao_tributaria_cofinsModel.cs
@@ -0,0 +1,50 @@
+﻿using HLP.Comum.Infrastructure;
+using HLP.Comum.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Entries.Model.Models.Fiscal
+{
+    public partial class Situacao_tributaria_cofinsModel : modelBase
+    {
+        public Situacao_tributaria_cofinsModel()
+            : base(xTabela: "Situacao_tributaria_cofins")
+        {
+        }
+
+
+        private int? _idCSTCofins;
+
+        [ParameterOrder(Order = 1)]
+        public int? idCSTCofins
+        {
+            get { return _idCSTCofins; }
+            set
+            {
+                _idCSTCofins = value;
+                base.NotifyPropertyChanged(propertyName: "idCSTCofins");
+            }
+        }
+
+        [ParameterOrder(Order = 2)]
+        public string cCSTCofins { get; set; }
+        [ParameterOrder(Order = 3)]
+        public string xCSTCofins { get; set; }
+        [ParameterOrder(Order = 4)]
+        public byte stSimplesNacional { get; set; }
+    }
+
+    public partial class Situacao_tributaria_cofinsModel
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                return base[columnName];
+            }
+        }
+    }
+}
