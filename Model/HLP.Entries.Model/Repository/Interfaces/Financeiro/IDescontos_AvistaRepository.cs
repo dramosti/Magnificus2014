0f275d1f198a52bb0603b1728d65c8d879085f7c
dramosti <diego@hlp.com.br>
2013-11-29 09:09:23 -0200

    Descontos_AvistaModel.cs

---
+++ Model/HLP.Entries.Model/Repository/Interfaces/Financeiro/IDescontos_AvistaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HLP.Entries.Model.Models.Financeiro;
+
+namespace HLP.Entries.Model.Repository.Interfaces.Financeiro
+{
+    public interface IDescontos_AvistaRepository
+    {
+        Descontos_AvistaModel GetDesconto(int idDescontosAvista);
+        List<Descontos_AvistaModel> GetAll();
+        void Save(Descontos_AvistaModel desconto);
+        void Delete(int idDescontosAvista);
+        int Copy(int idDescontosAvista);
+    }
+}
