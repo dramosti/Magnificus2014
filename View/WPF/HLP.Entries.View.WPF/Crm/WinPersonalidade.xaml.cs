1ad47add34b8c728a76f14cae67ace7a04a80ddc
LucasMazetto <lucas@hlp.com.br>
2013-12-05 10:38:48 -0200

    FormDesconto

---
+++ View/WPF/HLP.Entries.View.WPF/Crm/WinPersonalidade.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HLP.Comum.View.Formularios;
+
+
+namespace HLP.Entries.View.WPF.Crm
+{
+    /// <summary>
+    /// Interaction logic for WinPersonalidade.xaml
+    /// </summary>
+    public partial class WinPersonalidade : WindowsBase
+    {
+        public WinPersonalidade()
+        {
+            InitializeComponent();
+        }
+    }
+}
