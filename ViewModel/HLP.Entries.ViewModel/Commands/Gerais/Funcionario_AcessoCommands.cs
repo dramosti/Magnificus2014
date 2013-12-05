7d9df76bdc6ddf466c7b630279d1152630f3785a
armagni <mario@hlp.com.br>
2013-12-05 09:26:30 -0200

    Inclussão de wcf de acesso

--- ViewModel/HLP.Entries.ViewModel/Commands/Gerais/Funcionario_AcessoCommands.cs
+++ ViewModel/HLP.Entries.ViewModel/Commands/Gerais/Funcionario_AcessoCommands.cs
@@ -12,32 +12,7 @@ namespace HLP.Entries.ViewModel.Commands.Gerais
     {
         Funcionario_AcessoViewModel objViewModel = new Funcionario_AcessoViewModel();
         public Funcionario_AcessoCommands(Funcionario_AcessoViewModel objViewModel)
-        {
-            this.objViewModel = objViewModel;
-
-            this.objViewModel.commandDeletar = new RelayCommand(paramExec => Delete(),
-                    paramCanExec => DeleteCanExecute());
-
-            this.objViewModel.commandSalvar = new RelayCommand(paramExec => Save(),
-                    paramCanExec => SaveCanExecute(paramCanExec));
-
-            this.objViewModel.commandNovo = new RelayCommand(execute: paramExec => this.Novo(),
-                   canExecute: paramCanExec => this.NovoCanExecute());
-
-            this.objViewModel.commandAlterar = new RelayCommand(execute: paramExec => this.Alterar(),
-                    canExecute: paramCanExec => this.AlterarCanExecute());
-
-            this.objViewModel.commandCancelar = new RelayCommand(execute: paramExec => this.Cancelar(),
-                    canExecute: paramCanExec => this.CancelarCanExecute());
-
-            this.objViewModel.commandCopiar = new RelayCommand(execute: paramExec => this.Copy(),
-            canExecute: paramCanExec => this.CopyCanExecute());
-
-            this.objViewModel.commandPesquisar = new RelayCommand(execute: paramExec => this.ExecPesquisa(),
-                        canExecute: paramCanExec => true);
-
-            this.objViewModel.navegarCommand = new RelayCommand(execute: paramExec => this.Navegar(ContentBotao: paramExec),
-                canExecute: paramCanExec => objViewModel.navegarBaseCommand.CanExecute(paramCanExec));
+        {            
         }
     }
 }
