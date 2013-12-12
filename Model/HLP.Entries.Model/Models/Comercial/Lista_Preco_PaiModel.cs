e40c4d4f158557dec41de441eaec96b40d026596
armagni <mario@hlp.com.br>
2013-12-12 11:19:32 -0200

    Realocamento wcf listapreco

--- Model/HLP.Entries.Model/Models/Comercial/Lista_Preco_PaiModel.cs
+++ Model/HLP.Entries.Model/Models/Comercial/Lista_Preco_PaiModel.cs
@@ -57,8 +57,18 @@ namespace HLP.Entries.Model.Models.Comercial
         [ParameterOrder(Order = 15)]
         public int? idListaPrecoOrigem { get; set; }
 
+
+        private ObservableCollectionBaseCadastros<Lista_precoModel> _lLista_preco;
+
         public ObservableCollectionBaseCadastros<Lista_precoModel> lLista_preco
-            = new ObservableCollectionBaseCadastros<Lista_precoModel>();
+        {
+            get { return _lLista_preco; }
+            set
+            {
+                _lLista_preco = value;
+                base.NotifyPropertyChanged(propertyName: "lLista_preco");
+            }
+        }
     }
 
     public partial class Lista_precoModel : modelBase
