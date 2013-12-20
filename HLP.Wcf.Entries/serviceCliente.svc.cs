727f008d29dd398602a9ca5c83ed9d7fb6c6326f
dramosti <diego@hlp.com.br>
2013-12-20 11:29:21 -0200

    aaa

--- HLP.Wcf.Entries/serviceCliente.svc.cs
+++ HLP.Wcf.Entries/serviceCliente.svc.cs
@@ -98,11 +98,13 @@ namespace HLP.Wcf.Entries
 
         public int saveCliente(HLP.Entries.Model.Models.Comercial.Cliente_fornecedorModel objCliente)
         {
-
             try
             {
                 this.cliente_fornecedorRepository.BeginTransaction();
                 this.cliente_fornecedorRepository.Save(objCliente_fornecedor: objCliente);
+
+                objCliente.cliente_fornecedor_fiscal.idClienteFornecedor = (int)objCliente.idClienteFornecedor;
+
                 this.cliente_fornecedor_fiscalRepository.Save(objCliente_fornecedor_fiscal:
                     objCliente.cliente_fornecedor_fiscal);
 
--- Model/HLP.Entries.Model/Models/Comercial/Cliente_fornecedorModel.cs
+++ Model/HLP.Entries.Model/Models/Comercial/Cliente_fornecedorModel.cs
@@ -21,6 +21,7 @@ namespace HLP.Entries.Model.Models.Comercial
             this.lCliente_fornecedor_produto = new ObservableCollectionBaseCadastros<Cliente_fornecedor_produtoModel>();
             this.lCliente_fornecedor_representante = new ObservableCollectionBaseCadastros<Cliente_fornecedor_representanteModel>();
             this.cliente_fornecedor_fiscal = new Cliente_fornecedor_fiscalModel();
+            this.dCadastro = DateTime.Today;
         }
 
         private int? _idClienteFornecedor;
