958aee725caeb38fb96c5fc4feeab4303553b193
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-04-10 14:08:54 -0300

    Os Canal de Venda

---
+++ HLP.Wcf.Entries/Iwcf_CanalDeVenda.cs
@@ -0,0 +1,29 @@
+﻿using HLP.Entries.Model.Models.Comercial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HLP.Wcf.Entries
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Iwcf_CanalDeVenda" in both code and config file together.
+    [ServiceContract]
+    public interface Iwcf_CanalDeVenda
+    {
+
+        [OperationContract]
+        Canal_vendaModel GetObject(int id);
+
+        [OperationContract]
+        int SaveObject(Canal_vendaModel obj);
+
+        [OperationContract]
+        bool DeleteObject(int id);
+
+        [OperationContract]
+        int CopyObject(int id);
+        
+    }
+}
