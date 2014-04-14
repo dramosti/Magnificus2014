847264d33d1d0eec06d2a45e80e15e09d4e7c0b3
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-04-14 10:29:08 -0300

    Os de Condição Pagamento

---
+++ HLP.Entries.Services/Gerais/Condicao_PagamentoService.cs
@@ -0,0 +1,158 @@
+﻿using HLP.Base.ClassesBases;
+using HLP.Base.Static;
+using HLP.Entries.Model.Models.Gerais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Entries.Services.Gerais
+{
+    public class Condicao_PagamentoService
+    {
+        const string xTabela = "Condicao_Pagamento;";
+
+        HLP.Wcf.Entries.wcf_CondicaoPagamento serviceNetwork;
+        wcf_CondicaoPagamento.Iwcf_CondicaoPagamentoClient serviceWeb;
+
+        HLP.Wcf.Entries.wcf_CamposBaseDados serviceCamposBaseDadosNetwork;
+        wcf_CamposBaseDados.Iwcf_CamposBaseDadosClient serviceCamposBaseDadosWeb;
+
+        public Condicao_PagamentoService()
+        {
+            switch (Sistema.bOnline)
+            {
+                case StConnection.OnlineNetwork:
+                    {
+                        serviceNetwork = new HLP.Wcf.Entries.wcf_CondicaoPagamento();
+                        serviceCamposBaseDadosNetwork = new Wcf.Entries.wcf_CamposBaseDados();
+
+                        #region Validação
+
+                        foreach (string str in xTabela.Split(';').ToArray())
+                        {
+                            if (lCamposSqlNotNull._lCamposSql.Count(i => i.xTabela == str) == 0)
+                            {
+                                CamposSqlNotNullModel lCampos = new CamposSqlNotNullModel();
+                                lCampos.xTabela = str;
+                                lCampos.lCamposSqlModel = serviceCamposBaseDadosNetwork.getCamposNotNull(
+                                    xTabela: str);
+                                lCamposSqlNotNull.AddCampoSql(objCamposSqlNotNull: lCampos);
+                            }
+                        }
+
+                        #endregion
+                    }
+                    break;
+                case StConnection.OnlineWeb:
+                    {
+                        serviceWeb = new wcf_CondicaoPagamento.Iwcf_CondicaoPagamentoClient();
+                        serviceCamposBaseDadosWeb = new wcf_CamposBaseDados.Iwcf_CamposBaseDadosClient();
+
+                        #region Validação
+
+                        foreach (string str in xTabela.Split(';').ToArray())
+                        {
+                            if (lCamposSqlNotNull._lCamposSql.Count(i => i.xTabela == str) == 0)
+                            {
+                                CamposSqlNotNullModel lCampos = new CamposSqlNotNullModel();
+                                lCampos.xTabela = str;
+                                lCampos.lCamposSqlModel = serviceCamposBaseDadosNetwork.getCamposNotNull(
+                                    xTabela: str);
+                                lCamposSqlNotNull.AddCampoSql(objCamposSqlNotNull: lCampos);
+                            }
+                        }
+
+                        #endregion
+                    }
+                    break;
+                case StConnection.Offline:
+                default:
+                    {
+                    } break;
+            }
+        }
+
+        public Condicao_pagamentoModel GetObject(int id)
+        {
+            switch (Sistema.bOnline)
+            {
+                case StConnection.OnlineNetwork:
+                    {
+                        return this.serviceNetwork.GetObject(id: id);
+                    }
+                case StConnection.OnlineWeb:
+                    {
+                        return this.serviceWeb.GetObject(id: id);
+                    }
+                case StConnection.Offline:
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        public int SaveObject(Condicao_pagamentoModel obj)
+        {
+            switch (Sistema.bOnline)
+            {
+                case StConnection.OnlineNetwork:
+                    {
+                        return this.serviceNetwork.SaveObject(obj: obj);
+                    }
+                case StConnection.OnlineWeb:
+                    {
+                        return this.serviceWeb.SaveObject(obj: obj);
+                    }
+                case StConnection.Offline:
+                default:
+                    {
+                        return 0;
+                    }
+            }
+        }
+
+        public bool DeleteObject(int id)
+        {
+            switch (Sistema.bOnline)
+            {
+                case StConnection.OnlineNetwork:
+                    {
+                        return this.serviceNetwork.DeleteObject(id: id);
+                    }
+                case StConnection.OnlineWeb:
+                    {
+                        return this.serviceWeb.DeleteObject(id: id);
+                    }
+                case StConnection.Offline:
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+
+        public Condicao_pagamentoModel CopyObject(int id)
+        {
+            switch (Sistema.bOnline)
+            {
+                case StConnection.OnlineNetwork:
+                    {
+                        return this.serviceNetwork.CopyObject(id: id);
+                    }
+                case StConnection.OnlineWeb:
+                    {
+                        return this.serviceWeb.CopyObject(id: id);
+                    }
+                case StConnection.Offline:
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+    }
+
+}
---
+++ HLP.Wcf.Entries/wcf_CondicaoPagamento.svc.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using HLP.Entries.Model.Models.Gerais;
+using Ninject;
+using HLP.Dependencies;
+using HLP.Base.Static;
+using HLP.Entries.Model.Repository.Interfaces.Gerais;
+
+namespace HLP.Wcf.Entries
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "wcf_CondicaoPagamento" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select wcf_CondicaoPagamento.svc or wcf_CondicaoPagamento.svc.cs at the Solution Explorer and start debugging.
+
+
+    public class wcf_CondicaoPagamento : Iwcf_CondicaoPagamento
+    {
+        [Inject]
+        public ICondicao_pagamentoRepository condicao_pagamentoRepository { get; set; }
+
+        public wcf_CondicaoPagamento()
+        {
+            IKernel kernel = new StandardKernel(new MagnificusDependenciesModule());
+            kernel.Settings.ActivationCacheDisabled = false;
+            kernel.Inject(this);
+            Log.xPath = @"C:\inetpub\wwwroot\log";
+        }
+
+        public Condicao_pagamentoModel GetObject(int id)
+        {
+            try
+            {
+                return this.condicao_pagamentoRepository.GetCondicao(idCondicaoPagamento: id);
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog(xLog: ex.Message);
+                throw;
+            }
+        }
+
+        public int SaveObject(Condicao_pagamentoModel obj)
+        {
+            try
+            {
+                condicao_pagamentoRepository.Save(condicao: obj);
+                return obj.idCondicaoPagamento ?? 0;
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog(xLog: ex.Message);
+                throw;
+            }
+        }
+
+        public bool DeleteObject(int id)
+        {
+            try
+            {
+                condicao_pagamentoRepository.Delete(idCondicaoPagamento: id);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog(xLog: ex.Message);
+                return false;
+            }
+        }
+
+        public Condicao_pagamentoModel CopyObject(int id)
+        {
+            try
+            {
+                return this.condicao_pagamentoRepository.GetCondicao(idCondicaoPagamento:
+                    this.condicao_pagamentoRepository.Copy(idCondicaoPagamento: id));
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog(xLog: ex.Message);
+                throw new FaultException(reason: ex.Message);
+            }
+        }
+    }
+
+}
