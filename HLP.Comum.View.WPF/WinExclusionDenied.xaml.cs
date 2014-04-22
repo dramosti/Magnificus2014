7b8f62223bff048ac2f704cec46090cd91685225
LucasMazetto <lucas@hlp.com.br>
2014-04-22 09:13:01 -0300

    Ajustes

---
+++ HLP.Comum.View.WPF/WinExclusionDenied.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HLP.Comum.View.WPF
+{
+    /// <summary>
+    /// Interaction logic for WinExclusionDenied.xaml
+    /// </summary>
+    public partial class WinExclusionDenied : Window
+    {
+        public WinExclusionDenied()
+        {
+            InitializeComponent();
+        }
+    }
+}
