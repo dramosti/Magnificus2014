d454cee27d3eacdf8f75ab0f2e2ec843af128e74
dramosti <diego@hlp.com.br>
2014-04-25 15:14:18 -0300

    A

---
+++ HLP.Base/ClassesBases/MessageHlp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HLP.Base.ClassesBases
+{
+    public class MessageHlp
+    {
+        private bool _bSave = false;
+        public bool bSave
+        {
+            get { return _bSave; }
+            set { _bSave = value; }
+        }
+        public bool Save()
+        {
+
+            if (MessageBox.Show(messageBoxText: "Deseja salvar o registro?",
+                caption: "Salvar?", button: MessageBoxButton.YesNo, icon: MessageBoxImage.Question)
+                == MessageBoxResult.Yes)
+                return bSave = true;
+            else
+                return bSave = false;
+        }
+
+
+        public bool Excluir() 
+        {
+             if (MessageBox.Show(messageBoxText: "Deseja excluir o registro?",
+                    caption: "Excluir?", button: MessageBoxButton.YesNo, icon: MessageBoxImage.Question)
+                    == MessageBoxResult.Yes)
+                 return  true;
+             else
+                 return false;                
+        }
+
+        public void Excluido() 
+        {
+            MessageBox.Show(messageBoxText: "Cadastro excluido com sucesso!", caption: "Ok",
+                           button: MessageBoxButton.OK, icon: MessageBoxImage.Information);
+        }
+    }
+}
