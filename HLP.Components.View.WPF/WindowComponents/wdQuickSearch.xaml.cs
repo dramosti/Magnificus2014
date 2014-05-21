107389c86fe332658d795c73f46cc2ff7258b7e9
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-05-21 15:17:58 -0300

    Teste com messageerror

---
+++ HLP.Components.View.WPF/WindowComponents/wdQuickSearch.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HLP.Components.View.WPF.WindowComponents
+{
+    /// <summary>
+    /// Interaction logic for wdQuickSearch.xaml
+    /// </summary>
+    public partial class wdQuickSearch : Window
+    {
+        public wdQuickSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
