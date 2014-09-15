806327fac3ab25faa233e45a98bf202db2376434
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-09-15 15:53:43 -0300

    sync

--- Comum/HLP.Comum.Infrastructure/Controllers/AsynchronousPesquisaHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace HLP.Comum.Infrastructure.Controllers
-{
-   public delegate void AsynchronousPesquisaHandler();
-}
